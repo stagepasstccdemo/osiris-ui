fcec85dd4c4725700ed0d2148d7929066d239e80
fillipeags <fillipe484@gmail.com>
2023-04-09 18:53:30 -0300

    feat: customize design system styleguide tailwind settings

---
+++ src/theme/typography.ts
@@ -0,0 +1,23 @@
+module.exports = {
+  fontFamily: {
+    primary: ["Raleway", "sans-serif"],
+    secondary: ["Varela Round", "sans-serif"],
+    ternary: ["Raleway Dots", "cursive"],
+  },
+  fontSize: {
+    headlines: {
+      h1: "3.5rem",
+      h2: "3rem",
+      h3: "2.5rem",
+      h4: "2",
+      h5: "1.5rem",
+      h6: "1.25rem",
+    },
+    body: {
+      body: "1rem",
+      subtitle: "1rem",
+      caption: "0.75rem",
+      button: "0.875rem",
+    },
+  },
+};
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,36 +1,13 @@
+const colors = require('./src/theme/colors');
+const typography = require('./src/theme/typography');
+const shadows = require('./src/theme/shadows');
+
 /** @type {import('tailwindcss').Config} */
 export default {
   mode: "jit", // faster build times
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {
-      colors: {
-        "os-primary": {
-          100: "#F26A0F",
-          200: "#F59415",
-          300: "#FBD592",
-        },
-        "os-secondary": {
-          100: "#221B3C",
-          200: "#3889CB",
-          300: "#758296",
-        },
-        "os-ternary": {
-          100: "#34D0CB",
-          200: "#F600FF",
-          300: "#7D6FEC",
-        },
-        grays: {
-          100: "#FFFFFF",
-          200: "#EEEFFC",
-          300: "#D2D3E0",
-          400: "#858699",
-          500: "#6B6F76",
-          600: "#292A35",
-          700: "#151621",
-        },
-      },
-    },
+    extend: Object.assign({}, colors, typography, shadows),
   },
   plugins: [
     require('@tailwindcss/typography'),
