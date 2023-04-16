d1f5a72b32fba939fa04df7e386dda0a58f39ff3
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-04-15 23:51:26 -0300

    chore: make chakra-ui recognize tailwind styleguide

--- src/theme/typography.ts
+++ src/theme/typography.ts
@@ -1,23 +1,29 @@
 module.exports = {
   fontFamily: {
-    primary: ["Raleway", "sans-serif"],
-    secondary: ["Varela Round", "sans-serif"],
+    primary: ["Varela Round", "sans-serif"],
+    secondary: ["Raleway", "sans-serif"],
     ternary: ["Raleway Dots", "cursive"],
   },
   fontSize: {
     headlines: {
-      h1: ["3.25rem", "3.5rem"],
-      h2: ["2rem", "3rem"],
-      h3: "2.5rem",
-      h4: "2",
-      h5: "1.5rem",
-      h6: "1.25rem",
+      h1: ["3.25rem", "3.5rem", "4rem", "4.5rem"],
+      h2: ["2rem", "3rem", "3.5rem", "4rem"],
+      h3: ["2.5rem", "2.75rem", "3rem", "3.25rem"],
+      h4: ["2", "2.25rem", "2.5rem", "2.75rem"],
+      h5: ["1.5rem", "1.75rem", "2rem", "2.25rem"],
+      h6: ["1.25rem", "1.5rem", "1.75rem", "2rem"],
     },
     body: {
-      body: "1rem",
-      subtitle: "1rem",
-      caption: "0.75rem",
-      button: "0.875rem",
+      body: ["1rem", "1.25rem", "1.5rem", "1.75rem"],
+      subtitle: ["1rem", "1.25rem", "1.5rem", "1.75rem"],
+      caption: ["0.75rem", "1rem", "1.25rem", "1.5rem"],
+      button: ["0.875rem", "1rem", "1.125rem", "1.25rem"],
     },
+  },
+  fontWeight: {
+    light: 300,
+    regular: 400,
+    medium: 500,
+    bold: 700,
   },
 };
