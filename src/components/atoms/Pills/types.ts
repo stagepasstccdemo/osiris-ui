dc61039a924ce426ea7a454b3cc0da539e877402
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-04-16 21:04:46 -0300

    feat: set pills as a button

--- src/components/atoms/Pills/types.ts
+++ src/components/atoms/Pills/types.ts
@@ -2,4 +2,5 @@ export type ButtonProps = React.ButtonHTMLAttributes<HTMLButtonElement>;
 export type PillsProps = ButtonProps & {
   key: string;
   selected?: boolean;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement>) => void;
 };
