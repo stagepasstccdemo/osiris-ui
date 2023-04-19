65fbf14ec5e6fee8e4ca9ebd640e091a4d02e18d
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-04-19 12:02:59 -0300

    refactor: spreading ops on input component

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-nested-ternary */
 import { useState } from "react";
 import {
   Input as ChakraInput,
@@ -11,28 +10,39 @@ import ChakraWrapper from "@providers/ChakraWrapper";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 
 export const Input = ({ type, ...props }) => {
-  const [show, setShow] = useState(false);
-  const handleClick = () => setShow(!show);
-  const renderBasedOnType = () =>
-    show ? (
-      <AiOutlineEyeInvisible size={24} color="#EEEFFC" onClick={handleClick} />
+  const { name, register } = props;
+
+  const [visibility, setVisibility] = useState(false);
+  const handleToggleVisibility = () => setVisibility(!visibility);
+
+  const helperIcon = () =>
+    visibility ? (
+      <AiOutlineEyeInvisible
+        size={24}
+        color="#EEEFFC"
+        onClick={handleToggleVisibility}
+      />
     ) : (
-      <AiOutlineEye size={24} color="#EEEFFC" onClick={handleClick} />
+      <AiOutlineEye
+        size={24}
+        color="#EEEFFC"
+        onClick={handleToggleVisibility}
+      />
     );
 
   return (
     <ChakraWrapper>
       <InputGroup>
         <ChakraInput
-          type={type !== "password" ? type : show ? "text" : "password"}
-          id={props.name}
-          name={props.name}
-          {...props.register(props.name)}
+          id={name}
+          type={type !== "password" ? type : visibility ? "text" : "password"}
+          name={name}
+          {...register(name)}
           {...props}
         />
         {type === "password" && (
           <InputRightElement h="100%" mr="0.5rem">
-            {renderBasedOnType()}
+            {helperIcon()}
           </InputRightElement>
         )}
       </InputGroup>
