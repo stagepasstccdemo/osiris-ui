64d6e2d53bf5fe4b623d8e7a286d580e11a685c0
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-04-19 10:36:09 -0300

    feat: evolve input component to allow show/hide on passwords

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -1,10 +1,35 @@
-import { Input as ChakraInput } from "@chakra-ui/react";
+import { useState } from "react";
+import {
+  Input as ChakraInput,
+  InputGroup,
+  InputRightElement,
+} from "@chakra-ui/react";
+
 import ChakraWrapper from "@providers/ChakraWrapper";
 
-export const Input = ({ ...props }) => {
+import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
+
+export const Input = ({ type, ...props }) => {
+  const [show, setShow] = useState(false);
+  const handleClick = () => setShow(!show);
+
+  const renderBasedOnType = () =>
+    show ? (
+      <AiOutlineEyeInvisible size={24} color="#EEEFFC" onClick={handleClick} />
+    ) : (
+      <AiOutlineEye size={24} color="#EEEFFC" onClick={handleClick} />
+    );
+
   return (
     <ChakraWrapper>
-      <ChakraInput {...props} />
+      <InputGroup>
+        <ChakraInput type={show ? "text" : "password"} {...props} />
+        {type === "password" && (
+          <InputRightElement h="100%" mr="0.5rem">
+            {renderBasedOnType()}
+          </InputRightElement>
+        )}
+      </InputGroup>
     </ChakraWrapper>
   );
 };
