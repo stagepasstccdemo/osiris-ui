6c607757c95992fbabc95a988adfe7856ac7683a
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-07-24 18:49:52 -0300

    chore: change tailwind.config settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,10 @@
-/* eslint-disable global-require */
 const colors = require("./src/theme/colors");
 const typography = require("./src/theme/typography");
 const shadows = require("./src/theme/shadows");
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  mode: "jit", // faster build times
+  mode: "jit",
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
