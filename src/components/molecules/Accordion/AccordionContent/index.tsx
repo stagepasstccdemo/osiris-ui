9c54903184f8559c215657158dfe7f5137ec902a
Fillipe Gomes <fillipe.gomes@kartado.com.br>
2023-07-29 01:16:46 -0300

    feat: create Accordion component

--- src/components/molecules/Accordion/AccordionContent/index.tsx
+++ src/components/molecules/Accordion/AccordionContent/index.tsx
@@ -1,27 +1,31 @@
 import { AccordionPanel, Link } from "@chakra-ui/react";
 import { Flex, Text } from "@components/atoms";
+import { AccordionData } from "../types";
 
-export const AccordionContent = () => {
+export const AccordionContent = ({ data }: { data: AccordionData[] }) => {
+  const renderContent = () => {
+    return data.map((item: AccordionData) => (
+      <Link key={item.id} href={item.pathTo}>
+        <Text fontSize="1.5rem">{item.title}</Text>
+      </Link>
+    ));
+  };
+
   return (
     <AccordionPanel pb={4}>
       <Flex flexDirection="column" gap="2" ml="0.5rem">
-        <Link href="teste">
-          <Text fontSize="1.5rem">Top Music</Text>
-        </Link>
-        <Link href="teste">
-          <Text fontSize="1.5rem">Classical</Text>
-        </Link>
-
-        <Link href="teste">
-          <Text fontSize="1.5rem">Dance/Eletronic</Text>
-        </Link>
-        <Link href="teste">
-          <Text fontSize="1.5rem">Rock/Pop</Text>
-        </Link>
-        <Link href="teste">
-          <Text fontSize="1.5rem">Jazz/Blues</Text>
-        </Link>
+        {renderContent()}
       </Flex>
     </AccordionPanel>
   );
+};
+
+AccordionContent.defaultProps = {
+  data: [
+    {
+      id: 1,
+      title: "Accordion Content",
+      pathTo: "/",
+    },
+  ],
 };
--- src/components/molecules/Accordion/AccordionMenu/index.tsx
+++ src/components/molecules/Accordion/AccordionMenu/index.tsx
@@ -1,15 +1,22 @@
 import { AccordionButton, AccordionIcon } from "@chakra-ui/react";
 import { Box, Text } from "@components/atoms";
+import { AccordionData } from "../types";
 
-export const AccordionMenu = () => {
+type AccordionMenuProps = Pick<AccordionData, "title">;
+
+export const AccordionMenu = ({ title }: AccordionMenuProps) => {
   return (
     <Text as="h2" color="os-primary.100">
       <AccordionButton bgColor="gray.200" rounded="xl" fontSize="2rem">
         <Box as="span" flex="1" textAlign="left">
-          Music
+          {title}
         </Box>
         <AccordionIcon />
       </AccordionButton>
     </Text>
   );
+};
+
+AccordionMenu.defaultProps = {
+  value: "Accordion Menu",
 };
